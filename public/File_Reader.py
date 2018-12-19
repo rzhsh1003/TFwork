ba0f069e2712fd772e458cfa1acc9cfdf60aa19f
Renzhansheng <317975868@qq.com>
2018-12-19 16:06:36 +0800

    update 20181219

--- public/File_Reader.py
+++ public/File_Reader.py
@@ -7,6 +7,7 @@ import os,sys,time
 from xlutils.copy import copy
 from xlrd import open_workbook
 from ruamel import yaml
+import json
 
 class YamlReader:
     def __init__(self, yamlf):
@@ -25,7 +26,7 @@ class YamlReader:
         return self._data
 
     def updata(self,name,value):
-        # 如果是第一次调用data，读取yaml文档，否则直接返回之前保存的数据
+        # 修改yml值
         with open(self.yamlf) as f:
             content = yaml.load(f, Loader=yaml.RoundTripLoader)
             content.update({name: value})
@@ -33,9 +34,29 @@ class YamlReader:
             yaml.dump(content, nf, Dumper=yaml.RoundTripDumper)
 
 
-class SheetTypeError(Exception):
-    pass
+class JsonReader:
+    def __init__(self, Jsonf):
+        if os.path.exists(Jsonf):
+            self.Jsonf = Jsonf
+        else:
+            raise FileNotFoundError('文件不存在！')
+        self._data = None
 
+    @property
+    def data(self):
+        # 如果是第一次调用data，读取json文档，否则直接返回之前保存的数据
+        if not self._data:
+            with open(self.Jsonf, 'rb') as f:
+                self._data =json.load(f)  # load后是个generator，用list组织成列表
+        return self._data
+
+    def updata(self,name,value):
+        # 如果是第一次调用data，读取yaml文档，否则直接返回之前保存的数据
+        with open(self.Jsonf) as f:
+            content = json.load(f)
+            content[name] = value
+        with open(self.Jsonf, 'w') as nf:
+            json.dump(content, nf)
 
 class ExcelReader:
     """
@@ -103,6 +124,14 @@ class ExcelReader:
 
 
 if __name__ == '__main__':
+    
+    # #读取json方法
+    y = '../data/test.json'
+    reader = JsonReader(y)
+    reader.updata('name','2222')
+    print(reader.data)
+
+
     # #读取yml方法
     # y = '../config/config.yml'
     # reader = YamlReader(y)
@@ -110,11 +139,10 @@ if __name__ == '__main__':
     # print(reader.data)
 
     # 读取excle方法，以及字典元素遍历
-    import json
-    e = '../data/baidu.xlsx'
-    reader = ExcelReader(e, title_line=False).data
+    # e = '../data/baidu.xlsx'
+    # reader = ExcelReader(e, title_line=False).data
     # print(reader)
-    print(json.dumps(reader))
+    # print(json.dumps(reader))
     # for d in reader:
     #     print (d)
     #     print(dumps(d, indent=4))
